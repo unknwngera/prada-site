08f1dde446f316208c5def52baed9b9e117b2603
gera <zkingchannel@gmail.com>
2022-08-09 08:57:57 +0300

    added new arrivals for him and la pradina

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,6 +4,10 @@ import ProdImg1 from './product-1.png'
 import ProdImg2 from './product-2.png'
 import ProdImg3 from './product-3.png'
 import LightLayersImg from './light-layers.png'
+import ProdImg4 from './product-4.png'
+import ProdImg5 from './product-5.png'
+import ProdImg6 from './product-6.png'
+import LaPradinaImg from './la-pradina.png'
 import './Main.css'
 const Main = () => {
     return (
@@ -24,6 +28,22 @@ const Main = () => {
                 </div>
                 <div className="light-layers-img">
                     <img src={LightLayersImg} alt="Beautiful-girl-on-a-green-background" title="Beautiful girl on a green background" />
+                </div>
+            </div>
+            <div className="for-him">
+                <div className="for-him-title">NEW ARRIVALS FOR HIM</div>
+                <div className="for-him-products">
+                    <Product img={ProdImg4} colors='2' title='Cotton pique T-shirt' href='#' />
+                    <Product img={ProdImg5} colors='1' title='Cotton tobe' href='#' />
+                    <Product img={ProdImg6} colors='2' title='Double Match poplin shirt' href='#' />
+                </div>
+            </div>
+            <div className="la-pradina">
+                <div className="la-pradina-info">
+                    <div className="la-pradina-title">LA PRADINA</div>
+                    <div className="la-pradina-btn">
+                        <a href="#">EVERYTHING FOR HER</a>
+                    </div>
                 </div>
             </div>
         </div>
